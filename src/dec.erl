9ae0c10885404f3e870826dfeea06b8ed4efe146
Namdak Tonpa <maxim@synrc.com>
2019-07-12 02:12:54 +0300

    0.7.1

--- src/dec.erl
+++ src/dec.erl
@@ -7,7 +7,7 @@ stop(_)      -> ok.
 init([])     -> {ok, { {one_for_one, 5, 10}, []} }.
 start(_, _)  -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-pow(X,0) -> X;
+pow(X,0) -> 1;
 pow(X,Y) -> X * pow(X,Y-1).
 
 'mul'({A,B},{C,D}) -> norm({A+C,B*D}).
@@ -16,5 +16,11 @@ pow(X,Y) -> X * pow(X,Y-1).
 
 'norm'({0,B}) -> {0,B};
 'norm'({A,B}) when B rem 10 == 0 -> 'norm'({A-1,B div 10});
-'norm'(X) -> X.
+'norm'({A,B}) -> {0,B*pow(10,A)}.
+
+sub(A,B) -> nsub(norm(A),norm(B)).
+nsub({A,B},{C,D}) -> {0,B - D}.
+
+add(A,B) -> nadd(norm(A),norm(B)).
+nadd({A,B},{C,D}) -> {0,B + D}.
 
