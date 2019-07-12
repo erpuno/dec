b00eb678cab5e0f37bbc257eb10ab8ac5f2bcdfd
Namdak Tonpa <maxim@synrc.com>
2019-07-12 21:20:50 +0300

    wip

--- src/dec.erl
+++ src/dec.erl
@@ -7,9 +7,6 @@ stop(_) -> ok.
 init([]) -> {ok, { {one_for_one, 5, 10}, []} }.
 start(_, _)  -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-pow(X,0) -> 1;
-pow(X,Y) -> X * pow(X,Y-1).
-
 mul({A,B},{C,D}) -> norm({A+C,B*D}).
 
 'div'(A, B) -> 'div'(A, B, application:get_env(dec,precision,10)).
@@ -19,22 +16,26 @@ mul({A,B},{C,D}) -> norm({A+C,B*D}).
 div2(M, A, B ) when M =< 0 -> A div B + r((A rem B)*10 div B);
 div2(M, A, B) ->  (A div B)*pow(10,M) + div2(M-1, (A rem B)*10, B).
 
-r(N) when N > 5 -> 1;
-r(N)->0.
+sub({A,B},{C,D}) when A > C -> norm({A, B - D*pow(10,abs(A-C))});
+sub({A,B},{C,D}) -> norm({C, B*pow(10,abs(A-C)) - D}).
 
-max3(A,B,C) -> max(max(A,B),C).
+add({A,B},{C,D}) when A > C -> norm({A, B + D*pow(10,abs(A-C))});
+add({A,B},{C,D}) -> norm({C, B*pow(10,abs(A-C)) + D}).
 
 norm({0,B}) -> {0,B};
 norm({A,B}) when B rem 10 == 0 -> norm({A-1,B div 10});
 norm({A,B}) -> {A,B}.
 
-sub({A,B},{C,D}) when A > C -> norm({A, B - D*pow(10,abs(A-C))});
-sub({A,B},{C,D}) -> norm({C, B*pow(10,abs(A-C)) - D}).
+r(N) when N > 5 -> 1;
+r(N)->0.
 
-add({A,B},{C,D}) when A > C -> norm({A, B + D*pow(10,abs(A-C))});
-add({A,B},{C,D}) -> norm({C, D + B*pow(10,abs(A-C))}).
+pow(X,0) -> 1;
+pow(X,Y) -> X * pow(X,Y-1).
+
+max3(A,B,C) -> max(max(A,B),C).
 
 test() ->
+   {1, 31}           = dec:'add'({1,1}, {0,3}),
    {1, 2}            = dec:'div'({0,1}, {0,5}),
    {4, 625}          = dec:'div'({1,1}, {1,16}),
    {10, 6666666667}  = dec:'div'({0,2}, {0,3}),
