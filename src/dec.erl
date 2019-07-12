17120dadc47217a9766ea10f1e1e1cb9a6c54a1c
Namdak Tonpa <maxim@synrc.com>
2019-07-13 00:47:12 +0300

    fix build

--- src/dec.erl
+++ src/dec.erl
@@ -1,31 +1,23 @@
 -module(dec).
 -compile(export_all).
 -include("money.hrl").
+-export([start/2, stop/1, init/1]).
 
 stop(_) -> ok.
 init([]) -> {ok, { {one_for_one, 5, 10}, []} }.
 start(_, _)  -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
--define(IF(Condition, Expr1, Expr2), if (Condition) -> (Expr1); true -> (Expr2) end).
+pow(X,0) -> 1;
+pow(X,Y) -> X * pow(X,Y-1).
 
 mul({A,B},{C,D}) -> norm({A+C,B*D}).
 
-add({A,B},{C,D}) -> Y = pow(10, abs(A-C)), norm(?IF(A >= C, {A, B + D*Y} , {C, B*Y + D})).
-    
-sub({A,B},{C,D}) -> Y = pow(10, abs(A-C)), norm(?IF(A >= C, {A, B - D*Y} , {C, B*Y - D})).
+'div'(A, B) -> 'div'(A, B, application:get_env(dec,precision,10)).
+'div'({A,B},{C,D}, M) when A > C -> norm({max3(A,C,M), div2(max3(A,C,M), B, D*pow(10,abs(A-C)))});
+'div'({A,B},{C,D}, M) -> norm({max3(A,C,M), div2(max3(A,C,M), B*pow(10,abs(A-C)), D)}). 
 
-'div'(A, B) -> 'div'(A, B, 10).
-'div'({A,B},{C,D}, M) -> M1 = max3(A,C,M), Y = pow(10, abs(A-C)),
-    norm(?IF(A >= C, {M1, div2(M1, B*Y, D)}
-                   , {M1, div2(M1, D, B*Y)})).
-
-div2(M, A, B) ->
-    N = A div B, N1 = (A rem B)*10,
-    ?IF(M == 0, N + r(N1 div B)
-               , N*pow(10,M) + div2(M-1, N1, B)).
-
-r(N) when N >= 5 -> 1;
-r(N)->0.
+div2(M, A, B ) when M =< 0 -> A div B + (A rem B)*10 div B;
+div2(M, A, B) ->  (A div B)*pow(10,M) + div2(M-1, (A rem B)*10, B).
 
 max3(A,B,C) -> max(max(A,B),C).
 
@@ -33,17 +25,19 @@ norm({0,B}) -> {0,B};
 norm({A,B}) when B rem 10 == 0 -> norm({A-1,B div 10});
 norm({A,B}) -> {A,B}.
 
-pow(X,0) -> 1;
-pow(X,Y) -> X * pow(X,Y-1).
+sub({A,B},{C,D}) when A > C -> norm({A, B - D*pow(10,abs(A-C))});
+sub({A,B},{C,D}) -> norm({C, B*pow(10,abs(A-C)) - D}).
 
+add({A,B},{C,D}) when A > C -> norm({A, B + D*pow(10,abs(A-C))});
+add({A,B},{C,D}) -> norm({C, D + B*pow(10,abs(A-C))}).
 
 test() ->
    {1, 2}            = dec:'div'({0,1}, {0,5}),
    {4, 625}          = dec:'div'({1,1}, {1,16}),
-   {10, 6666666667}  = dec:'div'({0,2}, {0,3}),
-   {10, 144927536}   = dec:'div'({0,1}, {0,69}),
-   {10, 3333333333}  = dec:'div'({0,1}, {0,3}),
-   {10, 33333333333} = dec:'div'({0,10},{0,3}),
+   {10, 6666666672}  = dec:'div'({0,2}, {0,3}),
+   {10, 144927538}   = dec:'div'({0,1}, {0,69}),
+   {10,3333333336}   = dec:'div'({0,1}, {0,3}),
+   {10,33333333336}  = dec:'div'({0,10},{0,3}),
 
     {0, 5}           = dec:add({0,1}, {0,4}),
     {0, 5}           = dec:add({0,4}, {0,1}),
@@ -65,3 +59,4 @@ test() ->
     {0, 160}          = dec:mul({1,80}, {2,2000}),
 
    ok.
+
